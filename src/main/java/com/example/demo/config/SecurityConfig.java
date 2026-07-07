cbb6bb64f4eedb7f1d58f5e0a7a7dd127216dc71
leenanagaraj2804 <leenanagaraj2804@gmail.com>
2026-07-07 15:11:48 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5500",  "http://127.0.0.1:5500"
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5500",  "http://127.0.0.1:5500","https://leenanagaraj2804.github.io/"
 ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
